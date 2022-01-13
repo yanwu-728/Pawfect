86f4f8e211861cf7dfd2d0bd486bcdac12c98428
yanwu-728 <74961465+yanwu-728@users.noreply.github.com>
2022-01-12 22:15:18 -0500

    Added NewEvent.js and some skeleton code

---
+++ client/src/components/modules/NewEvent.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import "./NewEventInput.css";
+import { post } from "../../utilities";
+
+/**
+ * New Post is a parent component for all input components
+ *
+ * Proptypes
+ * @param {string} defaultText is the placeholder text
+ * @param {({location, breed, time, intro}) => void} onSubmit: (function) triggered when this post is submitted, takes {storyId, value} as parameters
+ */
+const NewEventInput = (props) => {
+    // const [location, setLocation] = useState(""); waiting for Google map API integration
+    const [breed, setBreed] = useState("");
+    const [time, setTime] = useState(Date.now);
+    const [intro, setIntro] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.onSubmit && props.onSubmit(value);
+        setValue("");
+    };
+
+    return (
+        <div>
+            {/* some HTML code */}
+        </div>
+    );
+}
