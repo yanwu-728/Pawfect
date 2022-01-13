e45f3ec77f01c6e8919e2f4bd40a1f83f9cf7e6f
yanwu-728 <74961465+yanwu-728@users.noreply.github.com>
2022-01-13 14:03:51 -0500

    Updated NewEvent.js to include descriptions

--- client/src/components/modules/NewEvent.js
+++ client/src/components/modules/NewEvent.js
@@ -9,36 +9,55 @@ import { post } from "../../utilities";
  *
  * Proptypes
  * @param {string} defaultText is the placeholder text
- * @param {({location, breed, time, intro}) => void} onSubmit: (function) triggered when this post is submitted, takes {storyId, value} as parameters
+ * @param {({location, breed, time, intro, noparticipant}) => void} onSubmit: (function) triggered when this post is submitted, takes {storyId, value} as parameters
  */
 const NewEventInput = (props) => {
     // const [location, setLocation] = useState(""); waiting for Google map API integration
     const [breed, setBreed] = useState(null);
     const [selectedDate, setDate] = useState(null);
     const [location, setLocation] = useState(null);
+    const [intro, setIntro] = useState("");
   
     const changeBreed = (event) => {
-      setBreed(event.target.value);
+        setBreed(event.target.value);
     };
   
     const changeDate = (date) => {
-      setDate(moment(date).format("MMMM Do YYYY"));
+        setDate(moment(date).format("MMMM Do YYYY"));
     }
 
     const changeLocation = (event) => {
         setLocation(event.target.value);
     }
 
+    const changeIntro = (event) => {
+        setIntro(event.target.value);
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        props.onSubmit && props.onSubmit(value);
-        setValue("");
+        props.onSubmit && props.onSubmit(location, breed, selectedDate, intro);
+        // setIntro("");
     };
 
     return (
         <div>
             <Filter changeBreed={changeBreed} changeDate={changeDate} changeLocation={changeLocation}/>
+            <input
+                type="text"
+                placeholder= "Please enter description"
+                value={intro}
+                onChange={changeIntro}
+            />
             <p>The event is on {selectedDate} at {location} with a {breed}.</p>
+            <p>Description: {intro}</p>
+            <button
+                type="submit"
+                value="Submit"
+                onClick={handleSubmit}
+            >
+                Submit
+            </button>
         </div>
     );
 };
