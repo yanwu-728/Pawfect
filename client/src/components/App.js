56ce72afaff9574d79cb226ab2c188ccb309429f
penny <zipeitan@mit.edu>
2022-01-14 19:19:17 -0500

    changed login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -44,7 +44,7 @@ const App = () => {
 
   return (
     <>
-      <NavBar userId={userId}/>
+      <NavBar handleLogin={handleLogin} handleLogout={handleLogout} userId={userId}/>
       <Router>
         <Skeleton path="/" handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
         <FindDog path="/finddog/" />
