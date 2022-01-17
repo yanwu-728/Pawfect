e5616db0b0613d03dd0d92fb9e2555ba19868c2d
hopenutshell <fuxinyi4444@gmail.com>
2022-01-17 03:00:17 +0000

    logo

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -13,7 +13,9 @@ const GOOGLE_CLIENT_ID = "237465931636-o4q19fum61608nv3mh3r299u8d4hq5qp.apps.goo
 const NavBar = ({ userId, handleLogin, handleLogout }) => {
   return (
     <nav className="NavBar-container">
-      <div className="NavBar-title u-inlineBlock">Pawfect</div>
+      <div class="logo"></div>
+      {/* 
+      <div className="NavBar-title u-inlineBlock">Pawfect</div> */}
       <div className="NavBar-linkContainer u-inlineBlock">
         {userId ? (
           <GoogleLogout
