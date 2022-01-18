787b8085cd7b8a06ac7ef57323a9d9c88114bed7
yanwu-728 <74961465+yanwu-728@users.noreply.github.com>
2022-01-17 20:08:37 -0500

    added .env file

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@
 // this is a tool provided by staff, so you don't need to worry about it
 const validator = require("./validator");
 validator.checkSetup();
+require('dotenv').config();
 
 //import libraries needed for the webserver to work!
 const http = require("http");
@@ -33,7 +34,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://URLgrey:URLgreyisthebest@cluster0.ptp86.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoConnectionURL = process.env.ATLAS_SRV;
 // TODO change database name to the name you chose
 const databaseName = "TeaCup";
 
@@ -57,7 +58,7 @@ app.use(express.json());
 // set up a session, which will persist login data across requests
 app.use(
   session({
-    secret: "session-secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
@@ -95,7 +96,7 @@ app.use((err, req, res, next) => {
 });
 
 // hardcode port to 3000 for now
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.Server(app);
 socketManager.init(server);
 
