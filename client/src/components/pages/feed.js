97afc10104101255e868f841651564b06227f4c3
penny <zipeitan@mit.edu>
2022-01-24 21:40:38 -0500

    changed feed

--- client/src/components/pages/feed.js
+++ client/src/components/pages/feed.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import DogCard from "../modules/DogCard.js";
 import { get } from "../../utilities";
 import "./feed.css";
+import SlideShow from "../Modules/SlideShow.js";
 
 const Feed = (props) => {
   const [user, setUser] = useState();
@@ -22,21 +23,30 @@ const Feed = (props) => {
           <h2 class="welcome">Hi! Welcome to PAWFECT! </h2>
           <h4 class="welcome">Please log in first.</h4>
         </div>
-        <div class="dogs" />;
+        {/* <div class="dogs" />; */}
+        <SlideShow userId={user}></SlideShow>
       </>
     );
   }
   return (
     <>
-      <div>
-        <h2 class="welcome">Hi {user.name}! </h2>
-        <h3 class="welcome">Welcome to PAWFECT!</h3>
-        <h4 class="welcome">
-          Please go to "Find a Dog!" page to see posted events or add your own on "My Schedule"
-          page!
-        </h4>
-      </div>
-      <div class="dogs" />;
+    <p>
+      <SlideShow></SlideShow>
+    </p>
+      <p>
+        <div class="welcome">
+          <h2>Hi {user.name}! </h2>
+          <h3>Welcome to PAWFECT!</h3>
+          <h4>
+            Please go to "Find a Dog!" page to see posted events or add your own on "My Schedule"
+            page!
+          </h4>
+        </div>
+        
+      </p>
+      {/* <div class="dogs" />; */}
+      
+
     </>
   );
 };
